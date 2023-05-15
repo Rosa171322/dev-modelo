270af575ae8cd9b0c28a83b6ca82c4c29ff395b4
Marco Mazzei <marco.mazzei@fatec.sp.gov.br>
2023-05-15 18:17:38 -0300

    Criação da classe de teste e inserção do tipo na classe Empregado.java

--- Empregado.java
+++ Empregado.java
@@ -36,5 +36,12 @@ public class Empregado {
         this.idade = idade;
     }
 
-    
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
 }
